cab41844d6d7e49df7f977ab25b77a78fd285216
Petteri Mäkelä <petteri.makela@visma.com>
2022-07-18 14:11:55 +0300

    Removing tests

--- src/components/Footer/__test__/footer.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { Footer } from '../Footer'
-import { render, screen } from '@testing-library/react'
-
-test('Footer renders', () => {
-    render(<Footer />)
-    const footer = screen.getByRole('contentinfo')
-    expect(footer).toBeTruthy()
-})
--- src/components/Header/__test__/headerImage.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { HeaderImage } from '../HeaderImage'
-import { render, screen } from '@testing-library/react'
-
-test('HeaderImage renders', () => {
-    render(<HeaderImage />)
-    const headerImage = screen.getByTestId('headerImage')
-    expect(headerImage).toBeTruthy()
-})
